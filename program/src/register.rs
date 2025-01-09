852702e9812500570ce1b8f290fce180966d1079
Hardhat Chad <hardhatchad@gmail.com>
2025-01-09 08:14:06 -0800

    remove debug logs

--- program/src/register.rs
+++ program/src/register.rs
@@ -9,22 +9,16 @@ pub fn process_register(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramRe
     let [signer_info, payer_info, proof_info, reservation_info, system_program] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
-    sol_log("A");
     signer_info.is_signer()?;
-    sol_log("B");
     payer_info.is_signer()?;
-    sol_log("C");
     let proof = proof_info
         .as_account::<Proof>(&ore_api::ID)?
         .assert(|p| p.miner == *signer_info.key)?;
-    sol_log("D");
     reservation_info
         .is_writable()?
         .is_empty()?
         .has_seeds(&[RESERVATION, proof_info.key.as_ref()], &ore_boost_api::ID)?;
-    sol_log("E");
     system_program.is_program(&solana_program::system_program::ID)?;
-    sol_log("F");
     
     // Create the reservation account.
     create_account::<Reservation>(
