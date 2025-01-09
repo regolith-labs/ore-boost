95d6b382b8dd8b3a78f4a75a7c8766daba4c83a8
Hardhat Chad <hardhatchad@gmail.com>
2025-01-09 08:09:31 -0800

    debug logs

--- program/src/register.rs
+++ program/src/register.rs
@@ -1,6 +1,6 @@
 use ore_api::state::Proof;
 use ore_boost_api::{consts::RESERVATION, state::Reservation};
-use solana_program::keccak::hashv;
+use solana_program::{keccak::hashv, log::sol_log};
 use steel::*;
 
 /// Registers a reservation account for a miner.
@@ -9,16 +9,22 @@ pub fn process_register(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramRe
     let [signer_info, payer_info, proof_info, reservation_info, system_program] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
+    sol_log("A");
     signer_info.is_signer()?;
+    sol_log("B");
     payer_info.is_signer()?;
+    sol_log("C");
     let proof = proof_info
         .as_account::<Proof>(&ore_api::ID)?
         .assert(|p| p.miner == *signer_info.key)?;
+    sol_log("D");
     reservation_info
         .is_writable()?
         .is_empty()?
         .has_seeds(&[RESERVATION, proof_info.key.as_ref()], &ore_boost_api::ID)?;
+    sol_log("E");
     system_program.is_program(&solana_program::system_program::ID)?;
+    sol_log("F");
     
     // Create the reservation account.
     create_account::<Reservation>(
