cf52536900b6e84b58e4b37b0c8e1a67b3f8d9f5
alex <aabinaei@gmail.com>
2025-02-13 18:14:39 -0800

    run

--- checkpoint/src/checkpoint.rs
+++ checkpoint/src/checkpoint.rs
@@ -20,14 +20,11 @@ pub async fn run_all(client: Arc<Client>) -> Result<()> {
     for b in boosts {
         let client = Arc::clone(&client);
         let handle = tokio::spawn(async move {
-            // if let Err(err) = run(client.as_ref(), &b.mint).await {
-            //     // log error then return
-            //     let (pda, _) = ore_boost_api::state::boost_pda(b.mint);
-            //     log::error!("{} -- exit", pda);
-            //     return Err(err);
-            // }
-            loop {
-                tokio::time::sleep(tokio::time::Duration::from_secs(6_000_000_000)).await;
+            if let Err(err) = run(client.as_ref(), &b.mint).await {
+                // log error then return
+                let (pda, _) = ore_boost_api::state::boost_pda(b.mint);
+                log::error!("{} -- exit", pda);
+                return Err(err);
             }
             Ok::<_, anyhow::Error>(())
         });
