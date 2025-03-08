30006a447ef12bcc81cbaa5fddf063005ae0a97b
Hardhat Chad <hardhatchad@gmail.com>
2025-03-08 10:02:28 -0800

    migration

--- program/src/migrate.rs
+++ program/src/migrate.rs
@@ -21,15 +21,17 @@ pub fn process_migrate(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramRes
     let boost = boost_info
         .as_account_mut::<Boost>(&ore_boost_api::ID)?
         .assert_mut(|b| b.mint == *mint_info.key)?;
-    let _boost_v1 = boost_v1_info
-        .as_account_mut::<Boost>(&ore_boost_api_v1::ID)? // TODO Parsing
-        .assert_mut(|b| b.mint == *mint_info.key)?;
+    let boost_v1 = boost_v1_info
+        .as_account::<ore_boost_api_v1::state::Boost>(&ore_boost_api_v1::ID)? // TODO Parsing
+        .assert(|b| b.mint == *mint_info.key)?;
     mint_info.as_mint()?;
     stake_info.is_empty()?.is_writable()?.has_seeds(
         &[STAKE, signer_info.key.as_ref(), boost_info.key.as_ref()],
         &ore_boost_api::ID,
     )?;
-    let stake_v1 = stake_v1_info.as_account_mut::<Stake>(&ore_boost_api_v1::ID)?; // TODO Parsing
+    let stake_v1 = stake_v1_info
+        .as_account::<ore_boost_api_v1::state::Stake>(&ore_boost_api_v1::ID)?
+        .assert(|s| s.id == boost.total_stakers)?; // TODO Parsing
     system_program.is_program(&system_program::ID)?;
 
     // Initialize the stake account.
@@ -50,16 +52,32 @@ pub fn process_migrate(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramRes
     stake.last_deposit_at = clock.unix_timestamp;
     stake.rewards = 0;
 
-    // Migrate account state.
-    stake.balance = stake_v1.balance;
-    stake.rewards = stake_v1.rewards;
+    // Accumulate raw rewards into rewards factor, to be divided by total deposits at end of migration.
+    boost.rewards_factor += Numeric::from_u64(stake_v1.rewards);
 
-    // Increment the total number of stakers.
+    // Update boost totals.
+    boost.total_deposits += stake_v1.balance;
     boost.total_stakers += 1;
 
-    // TODO: Migrate deposits balance.
+    // Update stake balance.
+    // Stake rewards balance is not updated here since it is captured by the boost rewards factor.
+    // It will be updated on the next claim/deposit/withdraw.
+    stake.balance = stake_v1.balance;
 
-    // TODO: Migrate rewards balance.
+    // Finish migration.
+    if boost.total_stakers == boost_v1.total_stakers {
+        let rewards = boost.rewards_factor.to_i80f48().to_num::<u64>();
+        boost.rewards_factor += Numeric::from_fraction(rewards, boost.total_deposits);
+    }
+
+    // TODO: Migrate deposits and rewards assets.
+    // invoke_signed(
+    // &ore_boost_api_v1::sdk::migrate(
+    //     *boost_info.key,
+    //     *stake_info.key,
+    //     stake_v1.balance,
+    // ),
+    // )?;
 
     Ok(())
 }
